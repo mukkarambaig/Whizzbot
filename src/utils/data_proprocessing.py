0d88dfa00549c2f75a7efc786aaf7787d0571041
mukkarambaig <mukkaram.baig@whizzbridge.com>
2024-03-11 18:12:43 +0500

    code fromatting

--- src/utils/data_proprocessing.py
+++ src/utils/data_proprocessing.py
@@ -20,42 +20,45 @@ CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", "100"))  # Providing default values
 CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP", "20"))
 EMBEDDING_MODEL_NAME = os.getenv("EMBEDDING_MODEL_NAME", "default-model-name")
 
+
 class TextSplitter:
     """Utility class to split text into chunks."""
-    
+
     def __init__(self, chunk_size: int, chunk_overlap: int):
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
         self.text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap
-        )
-    
+            chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap)
+
     def split(self, text: str) -> List[str]:
         """Split the text into chunks."""
         return self.text_splitter.create_documents([text])
 
+
 class VectorStoreManager:
     """Class to manage FAISS vectorstore operations."""
-    
+
     def __init__(self, embedding_model):
         self.embedding_model = embedding_model
-    
+
     def create_vectorstore(self, documents: List[str]):
         """Create and return a new FAISS vectorstore."""
-        return FAISS.from_documents(documents=documents, embedding=self.embedding_model)
-    
+        return FAISS.from_documents(documents=documents,
+                                    embedding=self.embedding_model)
+
     def save_vectorstore(self, vectorstore, vectorstore_path: str):
         """Save the FAISS vectorstore to a path."""
         vectorstore.save_local(vectorstore_path)
-    
+
     def load_vectorstore(self, vectorstore_path: str):
         """Load and return a FAISS vectorstore from a path."""
-        return FAISS.load_local(vectorstore_path, embeddings=self.embedding_model)
+        return FAISS.load_local(vectorstore_path,
+                                embeddings=self.embedding_model)
+
 
 def main():
     embedding_model = HuggingFaceInferenceAPIEmbeddings(
-        api_key=HF_API_KEY, model_name=EMBEDDING_MODEL_NAME
-    )
+        api_key=HF_API_KEY, model_name=EMBEDDING_MODEL_NAME)
     text_splitter = TextSplitter(CHUNK_SIZE, CHUNK_OVERLAP)
     vectorstore_manager = VectorStoreManager(embedding_model)
 
@@ -63,14 +66,15 @@ def main():
     text = read_unstructured_data(DOCUMENT_DIR)
     documents = text_splitter.split(text)
     vectorstore = vectorstore_manager.create_vectorstore(documents)
-    
+
     # Save FAISS vectorstore
     vectorstore_manager.save_vectorstore(vectorstore, EMBEDDINGS_DIR)
 
     # Load FAISS vectorstore
     loaded_vectorstore = vectorstore_manager.load_vectorstore(EMBEDDINGS_DIR)
-    
+
     print("Done")
+
 
 if __name__ == "__main__":
     main()
--- src/utils/llm_model.py
+++ src/utils/llm_model.py
@@ -13,29 +13,36 @@ load_dotenv()
 SERVICE_NAME = os.getenv("SERVICE_NAME")
 REGION_NAME = os.getenv("AWS_REGION")
 MODEL_ID = os.getenv("MODEL_ID")
-STREAMING = os.getenv(
-    "STREAMING",
-    "True").lower() == 'true'  # Convert to boolean
-MAX_GEN_LEN = int(os.getenv("MAX_GEN_LEN",))  # Convert to integer
+STREAMING = os.getenv("STREAMING",
+                      "True").lower() == 'true'  # Convert to boolean
+MAX_GEN_LEN = int(os.getenv("MAX_GEN_LEN", ))  # Convert to integer
 TEMPERATURE = float(os.getenv("TEMPERATURE"))  # Convert to float
 TOP_P = float(os.getenv("TOP_P"))  # Convert to float
 
 
-def initialize_boto3_client(service_name: str = SERVICE_NAME, region_name: str = REGION_NAME):
+def initialize_boto3_client(service_name: str = SERVICE_NAME,
+                            region_name: str = REGION_NAME):
     """Initialize and return a boto3 client for the specified service and region."""
     return boto3.client(service_name=service_name, region_name=region_name)
 
 
-def create_bedrock_instance(client, model_id: str = MODEL_ID, streaming: bool = STREAMING,
-                            max_gen_len: int = MAX_GEN_LEN, temperature: float = TEMPERATURE,
+def create_bedrock_instance(client,
+                            model_id: str = MODEL_ID,
+                            streaming: bool = STREAMING,
+                            max_gen_len: int = MAX_GEN_LEN,
+                            temperature: float = TEMPERATURE,
                             top_p: float = TOP_P):
     """Create and return a Bedrock instance with the specified parameters."""
-    return Bedrock(model_id=model_id, client=client, streaming=streaming,
-                    callbacks=[StreamingStdOutCallbackHandler()],
-                    model_kwargs={
-                        'max_gen_len': max_gen_len,
-                        'temperature': temperature,
-                        'top_p': top_p})
+
+    return Bedrock(model_id=model_id,
+                   client=client,
+                   streaming=streaming,
+                   callbacks=[StreamingStdOutCallbackHandler()],
+                   model_kwargs={
+                       'max_gen_len': max_gen_len,
+                       'temperature': temperature,
+                       'top_p': top_p
+                   })
 
 
 def format_docs(docs):
@@ -45,5 +52,7 @@ def format_docs(docs):
 
 def initialize_rag_chain(retriever, prompt, llm):
     """Initialize and return a RAG chain with the specified components."""
-    return ({"context": retriever | format_docs,
-             "question": RunnablePassthrough()} | prompt | llm | StrOutputParser())
+    return ({
+        "context": retriever | format_docs,
+        "question": RunnablePassthrough()
+    } | prompt | llm | StrOutputParser())
