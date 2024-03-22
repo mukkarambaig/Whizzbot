11163cf86394eed8220a1e4947b7d9f459d62089
mukkarambaig <mukkaram.baig@whizzbridge.com>
2024-03-22 16:54:26 +0500

    v4: Update

--- build/lib/utils/llm_model.py
+++ build/lib/utils/llm_model.py
@@ -4,12 +4,7 @@ import boto3
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.llms.bedrock import Bedrock
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableParallel, RunnablePassthrough
-from langchain.chains.question_answering import load_qa_chain
-from langchain.chains import ConversationChain, LLMChain
-from langchain.memory import ConversationSummaryBufferMemory
-from langchain.memory import ConversationBufferMemory
-from icecream import ic
+
 class BedrockManager:
     def __init__(self):
         # Load environment variables
@@ -23,7 +18,7 @@ class BedrockManager:
         self.max_gen_len = int(os.getenv("MAX_GEN_LEN", "512"))
         self.temperature = float(os.getenv("TEMPERATURE", "0.2"))
         self.top_p = float(os.getenv("TOP_P", "0.9"))
-
+        self.store = {} 
         self.client = self.initialize_boto3_client()
         self.bedrock_instance = self.create_bedrock_instance(self.client)
 
@@ -43,21 +38,6 @@ class BedrockManager:
                        model_kwargs={'max_gen_len': self.max_gen_len, 
                                      'temperature': self.temperature, 
                                      'top_p': self.top_p})
-    
-    def initialize_qa_chain(self):
-        """Initialize and return a question-answering chain."""
-        return load_qa_chain(llm=self.bedrock_instance)
-
-    def initialize_memory_conversation_chain(self):
-        """Initialize and return a memory conversation chain."""
-        return ConversationChain(llm=self.bedrock_instance, memory=ConversationSummaryBufferMemory(llm=self.bedrock_instance, max_token_limit=200),
-                                 verbose=True)
-
-    # FIXME: The following chain is not working. Expects prompt as argument.
-    def initialize_llm_chain(self):
-        """Initialize and return a language model chain."""
-        return LLMChain(llm=self.bedrock_instance, memory=ConversationBufferMemory(llm=self.bedrock_instance, max_token_limit=200),
-                                 verbose=True)
 
     def initialize_rag_chain(self):
         """Initialize and return a RAG chain with the specified components."""
